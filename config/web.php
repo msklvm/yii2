b365282ed4cf93d1faeda86c4b8385bf94bc5c4d
msklvm <moskal-333@mail.ru>
2022-01-25 11:18:43 +0700

    fix web

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'hjdhjkh4jkhjkghg455hjghjg',
+            'cookieValidationKey' => 'hjdhjkh4jkhjkghg455hjghjglk',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
